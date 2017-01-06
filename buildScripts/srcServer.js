ee7cf5221e47b478337f5676c748bd0873c009dc
piotrpawelzieba <piotr.pawel.zieba@outlook.com>
2017-01-06 19:15:15 +0100

    Added hot module reloading plugin

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
 
 const port = process.env.PORT || 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(webpackHotMiddleware(compiler));
+app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+
+}));
 
 app.get('/', (req, res) => res.sendFile(path.join( __dirname, '../src/index.html' )));
 
