78dee19e1792335da503520587d08530dd4cfa4f
piotrpawelzieba <piotr.pawel.zieba@outlook.com>
2017-01-06 19:17:25 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.css';
+import numeral from 'numeral';
+
+const courseValue = numeral(1000).format('$0,0.00');
+debugger;
+document.body.innerHTML = "aa"
+console.log(`You would pay ${courseValue} for this awesomeddddd course`);
