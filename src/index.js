212e129cd4fd34de74dd11a936ce981f91d2289f
piotrpawelzieba <piotr.pawel.zieba@outlook.com>
2017-01-07 19:38:11 +0100

    mocha installed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
-debugger;
-document.body.innerHTML = "aa"
-console.log(`You would pay ${courseValue} for this awesomeddddd course`);
+
+document.body.innerHTML = "abb"
+// console.log(`You would pay ${courseValue} for this awesomeddddd course`);
