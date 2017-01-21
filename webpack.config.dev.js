44dd5aea432ff0f2716b8cb79401c872e61e3acd
Piotr Zieba <1piotr.zieba1@gmail.com>
2017-01-21 15:20:44 +0100

    webpack react config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,28 +1,57 @@
 import path from 'path';
 import webpack from 'webpack';
 export default {
-  debug: true,
-  devtool: 'inline-source-map',
-  noInfo: false,
-  entry: [
-    'webpack-hot-middleware/client?reload=true',
-    path.resolve(__dirname, 'src/index')
-  ],
-  target: 'web',
-  output: {
-    path: path.resolve(__dirname, 'src'),
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-  ],
-  module: {
-    loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
-    ]
-  }
+    resolve:{
+        extensions: ["", ".js", ".jsx", ".json"]
+    },
+    debug: true,
+    devtool: 'inline-source-map',
+    noInfo: false,
+    entry: [
+        'webpack-hot-middleware/client?reload=true',
+        path.resolve(__dirname, 'src/index.js')
+    ],
+    target: 'web',
+    output: {
+        path: path.resolve(__dirname, 'src'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+    ],
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'react', 'latest']
+            }
+        }, {
+            test: /\.jsx$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'react', 'latest']
+            }
+        }, {
+            test: /\.css$/,
+            loaders: ['style','css']
+        },{
+            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: "url-loader?limit=10000&minetype=application/font-woff"
+        },{
+            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: "file-loader"
+        },{
+            test: /\.(jpg|png)$/,
+            loader: 'file-loader',
+            options: {
+                limit: 25000,
+            }
+        }]
+    }
 }
