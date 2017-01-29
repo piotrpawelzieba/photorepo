b877fd53bec9caa3a4a269401479ba31c99933d8
piotrpawelzieba <piotr.pawel.zieba@outlook.com>
2017-01-29 17:20:16 +0100

    components entry point implemented

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import GridView from './Library/GridView.js';
+import ListView from './Library/ListView.js';
+import Categories from './Library/Categories.js';
+
+
+export {
+    GridView,
+    ListView,
+    Categories
+}
