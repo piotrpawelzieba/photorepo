a94560a329a1a6fc1afec2ff89f0915457790384
piotrpawelzieba <piotr.pawel.zieba@outlook.com>
2017-02-19 21:10:11 +0100

    refactoring & implemented assigning category  & filtering view by categories funcionalities

--- src/components/Library/Categories.js
+++ src/components/Library/Categories.js
@@ -2,53 +2,63 @@ import React from 'react';
 import './Categories.css';
 import '../../../node_modules/font-awesome/css/font-awesome.css';
 import Locker from './Locker';
+import Category from './Category.jsx';
 
+const renderExistingCategories = (
+    categories, 
+    onDeleteClick, 
+    onCategoryClick, 
+    onDrop
+    ) => categories.map(
+        (category, pos) => <Category 
+                                category={category} 
+                                key={pos} 
+                                onDeleteClick={onDeleteClick} 
+                                onCategoryClick={onCategoryClick}
+                                onDrop={onDrop} />);
 
-const renderCategory = ({title, itemsCount},pos,  onDeleteClick) => (
-    <li key={pos+1} className={"categories__item"}>
-        <a className={'categories__link'} href={`#${title}`} >{`${title} (${itemsCount})`} </a>  
-        <a className="fa fa-trash-o" data-category={title} aria-hidden="true" onClick={onDeleteClick}></a>
+const renderNewCategoryButton = (showCategoryCreator) => (
+    <li key={0} className={"categories__item"} >
+        <a className={'categories__link'} href="#newCategory" onClick={showCategoryCreator}>
+            {'New Category'}
+        </a>
     </li>
 );
 
+const renderNewCategoryCreator = (categoryCreator, setTitle, onLockClick, onAddClick, onCancelClick) => (
+    <li className={"categories__item categories__newItem"} key={9999}> 
+        <input className="categories__input" type="text" name="newCategory" onChange={setTitle} value={categoryCreator.title} />
+        <span className="categories__icons"> 
+            <Locker className={'categories__icon'} isPrivate={categoryCreator.isPrivate} onLockClick={onLockClick} />
+            <a className="categories__icon fa fa-check" data-categoryTitle={categoryCreator.title} onClick={onAddClick} aria-hidden="true"></a>
+            <a className="categories__icon fa fa-times" onClick={onCancelClick} aria-hidden="true"></a>
+        </span>
+    </li>
+)
+
+const addCount = () => {
+    console.log('Add count!');
+}
+
 export default ({
         categories, 
-        categoryCreator: {
-            title="", 
-            visible,
-            isPrivate
-        },
+        categoryCreator,
         setTitle,
         showCategoryCreator,
         onDeleteClick,
+        onCategoryClick,
         onCancelClick,
         onAddClick,
-        onLockClick
+        onLockClick,
+        onDrop
 
     }) => (
         <div>
             <h2>{'Categories:'}</h2>
             <ul className='categories'>
-                <li key={0} className={"categories__item"}>
-                    <a className={'categories__link'} href="#newCategory" onClick={showCategoryCreator}>
-                        {'New Category'}
-                    </a>
-                </li>
-                {
-                    categories.map((category, pos) => renderCategory(category, pos, onDeleteClick))
-                }
-                { 
-                    visible && 
-                        <li className={"categories__item categories__newItem"} key={9999}> 
-                            <input className="categories__input" type="text" name="newCategory" onChange={setTitle} value={title} />
-                            <span className="categories__icons"> 
-                                <Locker isPrivate={isPrivate} onLockClick={onLockClick} />
-                                <a className="fa fa-check" data-categoryTitle={title} onClick={onAddClick} aria-hidden="true"></a>
-                                <a className="fa fa-times" onClick={onCancelClick} aria-hidden="true"></a>
-                            </span>
-                           
-                        </li>
-                }
+                    { renderNewCategoryButton(showCategoryCreator, ) }
+                    { renderExistingCategories(categories, onDeleteClick, onCategoryClick, onDrop)}
+                    { categoryCreator.visible && renderNewCategoryCreator(categoryCreator, setTitle, onLockClick, onAddClick, onCancelClick) }                           
             </ul>
         </div>
 );
