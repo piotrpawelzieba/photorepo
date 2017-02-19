a9cdce081d18f6f9e1dc3f262a74fe526d896396
piotrpawelzieba <piotr.pawel.zieba@outlook.com>
2017-02-19 21:10:25 +0100

    implemented updatePhoto api helper

--- src/api/photos.js
+++ src/api/photos.js
@@ -2,3 +2,7 @@ import axios from 'axios';
 import {API_ROOT_URL} from '../constants';
 
 export const fetchPhotos = () => axios.get(`${API_ROOT_URL}/api/photos`);
+export const updatePhoto = (id, payload) => axios.put(`${API_ROOT_URL}/api/photos`, {
+        ...payload,
+        id 
+});
