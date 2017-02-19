3ed291fc0350c3d93ee858e3762ec5d4cd0a03f1
piotrpawelzieba <piotr.pawel.zieba@outlook.com>
2017-02-19 21:11:24 +0100

    implemented className definition using props

--- src/components/Library/Locker.js
+++ src/components/Library/Locker.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function({isPrivate, onLockClick}) {
+export default function({isPrivate, onLockClick, className}) {
     if(isPrivate){
-        return <a className="fa fa-lock" onClick={onLockClick} aria-hidden="true"></a>;
+        return <a className={className + " fa fa-lock"} onClick={onLockClick} aria-hidden="true"></a>;
     } else {
-        return <a className="fa fa-unlock" onClick={onLockClick} aria-hidden="true"></a>;
+        return <a className={className + " fa fa-unlock"} onClick={onLockClick} aria-hidden="true"></a>;
     }
 }
