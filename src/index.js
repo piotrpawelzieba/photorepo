d66f6e4600c90afe8c4ae5b20a1cd6cb4b9cd408
piotrpawelzieba <piotr.pawel.zieba@outlook.com>
2017-03-05 13:03:20 +0100

    implementing dropzone for photos upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+
 import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+import ReduxToastr from 'react-redux-toastr'
+import Library from './containers/Library';
+import Dropzone from './containers/Dropzone';
+
+
 import {render} from 'react-dom';
 
-import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 
 /*      middlewares         */ 
@@ -12,8 +18,6 @@ import rootReducer from './reducers';
 import {getPhotos} from './actions/photoActions'
 import {getCategories} from './actions/categoriesActions'
 
-import ReduxToastr from 'react-redux-toastr'
-import Library from './containers/Library';
 
 import './index.css';
 const logger = createLogger();
@@ -21,11 +25,38 @@ const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 store.dispatch(getPhotos());
 store.dispatch(getCategories());
 
+
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.showDropzone = this.showDropzone.bind(this);
+        this.hideDropzone = this.hideDropzone.bind(this);
+        this.onDragLeave = this.onDragLeave.bind(this);
+        this.state = {
+            showDropzone: false
+        };
+    }   
+    showDropzone(ev){
+        const {dataTransfer: {types}} = ev;
+        const [type = ""] = types; 
+        if(type==="Files")
+            this.setState({showDropzone: true});
+    }
+    hideDropzone(){
+            this.setState({showDropzone: false});
+    }
+    onDragLeave(ev){
+        const {target, clientX, clientY} = ev;
+
+        if(!clientX && !clientY)
+            this.hideDropzone();
+    }
     render(){
         return (
             <Provider store={store} >
-                <div>
+                <div 
+                    onDragOver={this.showDropzone}
+                >
                     <ReduxToastr
                         timeOut={4000}
                         newestOnTop={false}
@@ -36,11 +67,19 @@ class App extends Component {
                         progressBar
                     />
                     <Library />
+                    <div >
+                        <Dropzone 
+                            onDragLeave={this.onDragLeave}
+                            showDropzone={this.state.showDropzone}    
+                            hideDropzone={this.hideDropzone}
+                        />
+                    </div>
                 </div>
             </Provider>
         );
     }
 }
+
 
 const container = document.createElement('div');
 render(<App />, container);
