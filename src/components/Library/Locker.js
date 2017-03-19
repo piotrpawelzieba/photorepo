9efe9a749ee3281647f4a47b48143ae22f42deec
piotrpawelzieba <piotr.pawel.zieba@outlook.com>
2017-03-19 11:02:43 +0100

    categories moved to seperate container

--- src/components/Library/Locker.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function({isPrivate, onLockClick, className}) {
-    if(isPrivate){
-        return <a className={className + " fa fa-lock"} onClick={onLockClick} aria-hidden="true"></a>;
-    } else {
-        return <a className={className + " fa fa-unlock"} onClick={onLockClick} aria-hidden="true"></a>;
-    }
-}
