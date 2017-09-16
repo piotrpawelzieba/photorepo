2fb41759f90e628bfaf84e08cea00d1d3d5837dc
piotrpawelzieba <piotr.pawel.zieba@outlook.com>
2017-09-16 14:52:00 +0200

    moved responsibility for routing to the client

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -5,21 +5,20 @@ import webpack from 'webpack';
 import config from '../webpack.config.dev';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-
-/*eslint-disable no-console*/ 
+/*eslint-disable no-console*/
 
 const port = process.env.PORT || 3000;
 const app = express();
 const compiler = webpack(config);
 
+
 app.use(webpackHotMiddleware(compiler));
 app.use(webpackDevMiddleware(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath,
-
 }));
 
-app.get('/', (req, res) => res.sendFile(path.join( __dirname, '../src/index.html' )));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '../src/index.html')));
 
 app.listen(port, (err) => {
     err ? console.log(err) : open('http://localhost:' + port);
