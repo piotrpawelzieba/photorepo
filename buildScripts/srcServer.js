dd9e433e5105fda3567141917ce47da990f02c2f
piotrpawelzieba <piotr.pawel.zieba@outlook.com>
2018-04-08 17:30:50 +0200

    Upgraded webpack to 4.5.0

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,29 +1,32 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
 import express from 'express';
 import path from 'path';
 import open from 'open';
 import webpack from 'webpack';
-import config from '../webpack.config.dev';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-/*eslint-disable no-console*/
+import config from '../webpack.config.dev';
 
 const port = process.env.PORT || 3000;
 const app = express();
 const compiler = webpack(config);
 
-
 app.use(webpackHotMiddleware(compiler));
-app.use(webpackDevMiddleware(compiler, {
+app.use(
+  webpackDevMiddleware(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath,
-}));
+  }),
+);
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '../src/index.html')));
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../src/index.html')),
+);
 
-
-app.listen(port, (err) => {
-    err ? console.log(err) : open('http://localhost:' + port);
+app.listen(port, err => {
+  if (err) {
+    return console.log(err);
+  }
+  return open(`http://localhost:${port}`);
 });
-
-
-
